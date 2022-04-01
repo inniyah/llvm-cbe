6a26311190161c436ff5140f653e450925ad6e1c
SusanTan <zujunt@princeton.edu>
2022-04-01 16:41:36 -0400

    fix no store

--- lib/Target/CBackend/CBackend.cpp
+++ lib/Target/CBackend/CBackend.cpp
@@ -909,7 +909,7 @@ void CWriter::preprossesPHIs2Print(Function &F){
 
       if(ldPtr && stPtr && ldPtr == stPtr){
         skipInstsForPhis.insert(cast<Instruction>(ldInst));
-        skipInstsForPhis.insert(cast<Instruction>(stInst));
+        //skipInstsForPhis.insert(cast<Instruction>(stInst));
         phis2print[phi] = ldInst;
       }
 
